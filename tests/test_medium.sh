02608016e903a4ea144b4afd96931fca37e8c1e1
Sasha-v-tanke <93387431+Sasha-v-tanke@users.noreply.github.com>
2026-09-15 16:55:02 +0300

    add base ci

--- tests/test_medium.sh
+++ tests/test_medium.sh
@@ -2,6 +2,11 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+if [[ ! -d "${SCRIPT_DIR}/result" ]]; then
+    mkdir "${SCRIPT_DIR}/result/"
+fi
+TEST_LOG="${SCRIPT_DIR}/result/medium.log"
+: >"${TEST_LOG}"
 # shellcheck source=common.sh
 source "${SCRIPT_DIR}/common/common.sh"
 
--- tests/test_small.sh
+++ tests/test_small.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+if [[ ! -d "${SCRIPT_DIR}/result" ]]; then
+    mkdir "${SCRIPT_DIR}/result/"
+fi
 TEST_LOG="${SCRIPT_DIR}/result/small.log"
 : >"${TEST_LOG}"
 
